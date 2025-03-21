c76704905b7356ba447bcda6bb05c8ea631d09ce
paunchymonk <gregory@dickison.net>
2025-03-21 09:53:36 -0700

    Create dynamic categories route and dump it to a generic page

---
+++ src/app/categories/[tag]/page.js
@@ -0,0 +1,7 @@
+export default function CategoryPage() {
+  return (
+    <div className="pt-24 flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">CategoryPage</h1>
+    </div>
+  )
+}
