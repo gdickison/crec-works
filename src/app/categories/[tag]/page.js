e396bfca525305b95e38d4dd11453d794df313b5
paunchymonk <gregory@dickison.net>
2025-03-21 09:55:00 -0700

    Dump tag param on page

--- src/app/categories/[tag]/page.js
+++ src/app/categories/[tag]/page.js
@@ -1,7 +1,9 @@
-export default function CategoryPage() {
+export default function CategoryPage({params}) {
+  const {tag} = params
   return (
     <div className="pt-24 flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">CategoryPage</h1>
+      <h1 className="text-4xl font-bold">Category Page</h1>
+      <p className="text-2xl font-bold">{tag}</p>
     </div>
   )
 }
