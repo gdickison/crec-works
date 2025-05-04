5d05e2ee235061971b5dbfcc80b670bde2ef9a43
paunchymonk <gregory@dickison.net>
2025-05-03 17:44:55 -0700

    Wrap listings in UserListings in a Link - currenty 404

--- src/components/UserListings.js
+++ src/components/UserListings.js
@@ -1,5 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
+
+import Link from "next/link";
 import { useState, useEffect } from "react";
 import { getUserListings } from "@/app/actions";
 
@@ -9,7 +11,7 @@ export default function UserListings({user}) {
 
   useEffect(() => {
     const fetchListings = async () => {
-      const listings = await getUserListings(user.id);
+      const listings = await getUserListings('user_2tm13qfEsSNAkhxDuIqaZunvU8b');
       setListings(listings);
     };
     fetchListings();
@@ -25,16 +27,18 @@ export default function UserListings({user}) {
       ) : (
         <ul role="list" className="w-full">
           {listings.map((listing) => (
-            <li key={listing.id} className="my-4 hover:scale-105 transition-all duration-300">
-              <div className="flex items-center gap-12 w-full h-24 bg-white rounded-lg shadow-md p-4">
-                <img
-                  src={`${listing.imageFile ? `https://my-church-works-images.b-cdn.net/${listing.imageFile}` : `https://picsum.photos/id/${Math.floor(Math.random() * 500) + 1}/200`}`}
-                  alt={`Image ${listing.id}`}
-                  className="object-contain h-full rounded-md"
-                />
-                <h2 className="text-base/7 font-semibold tracking-tight text-gray-900">{listing.business_name}</h2>
-              </div>
-            </li>
+            <Link href={`/listings/${listing.id}`} key={listing.id}>
+              <li key={listing.id} className="my-4 hover:scale-105 transition-all duration-300">
+                <div className="flex items-center gap-12 w-full h-24 bg-white rounded-lg shadow-md p-4">
+                  <img
+                    src={`${listing.imageFile ? `https://my-church-works-images.b-cdn.net/${listing.imageFile}` : `https://picsum.photos/id/${Math.floor(Math.random() * 500) + 1}/200`}`}
+                    alt={`Image ${listing.id}`}
+                    className="object-contain h-full rounded-md"
+                  />
+                  <h2 className="text-base/7 font-semibold tracking-tight text-gray-900">{listing.business_name}</h2>
+                </div>
+              </li>
+            </Link>
           ))}
         </ul>
       )}
