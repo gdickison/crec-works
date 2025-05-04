33c64ee7019c1ca4bd7bef88c41586dee9579115
paunchymonk <gregory@dickison.net>
2025-05-03 17:37:26 -0700

    Add listings to the UserListings component

--- src/components/UserListings.js
+++ src/components/UserListings.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 'use client';
 import { useState, useEffect } from "react";
 import { getUserListings } from "@/app/actions";
@@ -17,13 +18,25 @@ export default function UserListings({user}) {
   console.log('listings',listings);
 
   return (
-    <div className="h-full w-full flex flex-col items-center justify-center">
-      <h1>My Listings</h1>
-      {listings.length === 0 && (
-        <p>You have no listings</p>
-      )}
-      {listings.length > 0 && (
-        <p>{`You have ${listings.length} listing${listings.length === 1 ? '' : 's'}`}</p>
+    <div className="h-full w-full flex flex-col items-start justify-center">
+      <h1 className="text-2xl font-bold">My Listings</h1>
+      {listings.length === 0 ? (
+        <p className="text-gray-500 my-4">You have no listings</p>
+      ) : (
+        <ul role="list" className="w-full">
+          {listings.map((listing) => (
+            <li key={listing.id} className="my-4 hover:scale-105 transition-all duration-300">
+              <div className="flex items-center gap-12 w-full h-24 bg-white rounded-lg shadow-md p-4">
+                <img
+                  src={`${listing.imageFile ? `https://my-church-works-images.b-cdn.net/${listing.imageFile}` : `https://picsum.photos/id/${Math.floor(Math.random() * 500) + 1}/200`}`}
+                  alt={`Image ${listing.id}`}
+                  className="object-contain h-full rounded-md"
+                />
+                <h2 className="text-base/7 font-semibold tracking-tight text-gray-900">{listing.business_name}</h2>
+              </div>
+            </li>
+          ))}
+        </ul>
       )}
     </div>
   )
